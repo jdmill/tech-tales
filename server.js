dbc57223e745b07c66df632adfacb407c21ad032
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-09 18:42:18 -0400

    added a few UI changes

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
+//session object
 const sess = {
   secret: "Super secret secret",
   cookie: {},
